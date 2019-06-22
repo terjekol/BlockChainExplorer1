390d0577642b863acb0c9bc73e0e96ee6a715877
Terje Kolderup <terje@kolderup.net>
2019-06-22 10:30:02 +0200

    x

---
+++ BlockChainExplorer1/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Info.Blockchain.API.BlockExplorer;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlockChainExplorer1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private object _object;
+
+        public async void OnGet()
+        {
+            var explorer = new BlockExplorer();
+            var latestBlock = await explorer.GetLatestBlockAsync();
+            _object = await explorer.GetBlockByHashAsync(latestBlock.Hash);
+        }
+
+        public PropertyInfo[] Props => _object.GetType().GetProperties(BindingFlags.Public);
+        public string GetValue(PropertyInfo prop) => prop.GetValue(_object).ToString();
+    }
+}
