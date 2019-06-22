a814a4c437f985d593bceb8da46f26356af12245
Terje Kolderup <terje@kolderup.net>
2019-06-22 11:27:01 +0200

    Update Index.cshtml.cs

--- BlockChainExplorer1/Pages/Index.cshtml.cs
+++ BlockChainExplorer1/Pages/Index.cshtml.cs
@@ -1,5 +1,11 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Info.Blockchain.API.BlockExplorer;
+using Info.Blockchain.API.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BlockChainExplorer1.Pages
@@ -7,15 +13,32 @@ namespace BlockChainExplorer1.Pages
     public class IndexModel : PageModel
     {
         private object _object;
+        public PropertyInfo[] Props { get; private set; }
 
-        public async void OnGet()
+        public async Task OnGet()
         {
             var explorer = new BlockExplorer();
             var latestBlock = await explorer.GetLatestBlockAsync();
-            _object = await explorer.GetBlockByHashAsync(latestBlock.Hash);
+            var block = await explorer.GetBlockByHashAsync(latestBlock.Hash);
+            Save(block);
         }
 
-        public PropertyInfo[] Props => _object.GetType().GetProperties(BindingFlags.Public);
-        public string GetValue(PropertyInfo prop) => prop.GetValue(_object).ToString();
+        private void Save(object obj)
+        {
+            var type = obj.GetType();
+            Props = type.GetProperties();
+            _object = obj;
+        }
+
+        public string GetValue(PropertyInfo prop)
+        {
+            var value = prop.GetValue(_object);
+            if (prop.Name == "Transactions")
+            {
+                var transactions = (IEnumerable<object>)value;
+                return string.Join(',', transactions.Select(t=>((Transaction)t).Index));
+            }
+            return value.ToString();
+        }
     }
 }
